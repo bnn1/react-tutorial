a46321dac9a7772d54f79a2c8e7f3b564fe485bd
Boris Novikov <boris.novikov@protonmail.com>
2020-11-17 00:32:35 +0300

    refactoring - use map to display books

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React from 'react';
-import { book1, book2, book3 } from './BOOKS.json';
+
+// External file with all the books - BOOKS.json
+import BOOKS from './BOOKS.json';
 
 // CSS
 import './App.css';
-// variables
-const firstBook = {
-  imgSrc:
-    'https://images-na.ssl-images-amazon.com/images/I/71c1LRLBTBL._AC_UL200_SR200,200_.jpg',
-  title: 'Dog Man',
-  author: 'Dav Pilkey',
-};
-const secondBook = {
-  imgSrc:
-    'https://images-na.ssl-images-amazon.com/images/I/91nSi4sI-SL._AC_UL200_SR200,200_.jpg',
-  title: '5.000 amazing things',
-  author: 'National Geographic Kids',
-};
+
 const App = () => {
   return (
     <section className='book-list'>
-      <Book imgSrc={book1.imgSrc} title={book1.title} author={book1.author} />
-      <Book imgSrc={book2.imgSrc} title={book2.title} author={book2.author} />
-      <Book imgSrc={book3.imgSrc} title={book3.title} author={book3.author} />
+      {BOOKS.map((book) => {
+        return <Book book={book} />;
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
-  let { imgSrc, title, author } = props;
+  let { imgSrc, title, author } = props.book;
   return (
     <article className='book'>
       <img src={imgSrc} alt='' />
